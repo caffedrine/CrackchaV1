48f409fe988e2d64c9be495004438601bb06de22
caffedrine <bacchusstyle@yahoo.com>
2019-10-31 20:39:00 +0200

    LAst local changes.

--- main.py
+++ main.py
@@ -21,15 +21,15 @@ def get_new_hackthis_web_session():
     return sess
 
 
-def get_captcha(webSession, outputPathName = None):
-    webSession.get("https://www.hackthis.co.uk/levels/captcha/1", timeout=20)
-    response = webSession.get("https://www.hackthis.co.uk/levels/extras/captcha1.php", timeout=20)
+def get_captcha(web_session, outputPathName=None):
+    web_session.get("https://www.hackthis.co.uk/levels/captcha/1", timeout=20)
+    response = web_session.get("https://www.hackthis.co.uk/levels/extras/captcha1.php", timeout=20)
     if outputPathName is not None:
-    	with open(outputPathName, "wb") as fd:
-        	fd.write(response.content)
+        with open(outputPathName, "wb") as fd:
+            fd.write(response.content)
     else:
-    	with open("captcha.png", "wb") as fd:
-        	fd.write(response.content)
+        with open("captcha.png", "wb") as fd:
+            fd.write(response.content)
 
 
 def fetch_training_data():
@@ -48,76 +48,61 @@ def make_clean():
         try:
             if os.path.isfile(file_path):
                 os.unlink(file_path)
-            elif os.path.isdir(file_path): shutil.rmtree(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
         except Exception as e:
             print(e)
 
+
 def log(strToLog):
-	st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
-	print( "[%s] %s" % ( str(st), str(strToLog)), flush=True)
+    st = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
+    print("[%s] %s" % (str(st), str(strToLog)), flush=True)
 
 
 def get_user_solution(captchaName):
-	while True:
-		data = input("Please enter solution for '%s': " % (captchaName))
-		if len(data) < 10:
-		   	print("Value too short. Please inser all captcha chars!")
-		else:
-			return data.rstrip("\n\r")
+    while True:
+        data = input("Please enter solution for '%s': " % (captchaName))
+        if len(data) < 10:
+            print("Value too short. Please inser all captcha chars!")
+        else:
+            return data.rstrip("\n\r")
 
 
-def build_training_data(webSession):
+def build_training_data(web_session):
     counter = 0
     while True:
         i = 0
-    	# Get neax available captcha directory name
+        # Get near available captcha directory name
         while os.path.exists('traindata/captcha' + str(i)):
             i += 1
-		
-		# create a new dir 
+
+        # create a new dir
         os.makedirs('traindata/captcha' + str(i))
 
-    	# Captcha paths
-        captcha_path = "traindata/captcha%s/0_captcha%s.png" % (str(i), str(i))	
+        # Captcha paths
+        captcha_path = "traindata/captcha%s/0_captcha%s.png" % (str(i), str(i))
         captcha_clean_path = "traindata/captcha%s/0_captcha%s_clean.png" % (str(i), str(i))
 
         # Download captcha
-        get_captcha(webSession, captcha_path )
+        get_captcha(web_session, captcha_path)
 
-    	# Remove black background from image
+        # Remove black background from image
         ImageFilter.clean(captcha_path, captcha_clean_path)
 
-		# Ask user for solution of current captcha
+        # Ask user for solution of current captcha
         solution = get_user_solution(captcha_path)
 
         # Split captcha in chars and ask for solution
-        splitStatus = ImageSpliter.ImgSplit(captcha_clean_path, solution)
-
-
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in 
-    this function because it is programmed to be pretty 
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
+        split_status = ImageSpliter.ImgSplit(captcha_clean_path, solution)
 
 
 def main():
-	# Create a web session
+    # Create a web session
     log("Creating a new hackthis.co.uk web session...")
-    webSession = get_new_hackthis_web_session()
+    web_session = get_new_hackthis_web_session()
 
-	# Only to train the network
-	# build_training_data(webSession)
+    # Only to train the network
+    # build_training_data(web_session)
 
     # Clean guessdata directory
     log("Cleaning up old files...")
@@ -125,7 +110,7 @@ def main():
 
     # Fetch a new captcha
     log("Download new captcha...")
-    get_captcha(webSession, "guessdata/captcha.png")
+    get_captcha(web_session, "guessdata/captcha.png")
 
     # Remove black background from image
     log("Cleaning up image...")
@@ -140,32 +125,30 @@ def main():
     solution = ""
     for letter_image in natsorted(glob.glob("guessdata/*.png")):
         if "captcha" in letter_image:
-    	    continue
+            continue
         digest1 = imagehash.phash(Image.open(letter_image))
         for letter_image2 in glob.glob("traindata/**/*.png"):
             digest2 = imagehash.phash(Image.open(letter_image2))
             if digest1 == digest2:
                 letter = (os.path.basename(letter_image2).split("_")[0])
                 if letter in ImageSpliter.ILLEGAL_SYMBOLS.values():
-                    letter =  list(ImageSpliter.ILLEGAL_SYMBOLS.keys())[list(ImageSpliter.ILLEGAL_SYMBOLS.values()).index( letter )]  
+                    letter = list(ImageSpliter.ILLEGAL_SYMBOLS.keys())[list(ImageSpliter.ILLEGAL_SYMBOLS.values()).index(letter)]
                 solution += letter
                 break
         else:
             solution += "8"
 
     log("Submitting solution: '%s' " % (solution))
-    response = webSession.post("https://www.hackthis.co.uk/levels/captcha/1", data={"answer": solution } , timeout=20).content
-    
+    response = web_session.post("https://www.hackthis.co.uk/levels/captcha/1", data={"answer": solution}, timeout=20).content
+
     if "Incomplete" not in str(response):
         log("SUCCESS!")
     else:
-        log("FAILED!") # Time remaining: " + str(time_remaining) )
-        
+        log("FAILED!")
+
 
 if __name__ == '__main__':
-	try:
-		main()
-	except Exception as e:
-	    logging.error(traceback.format_exc())
-
-
+    try:
+        main()
+    except Exception as e:
+        logging.error(traceback.format_exc())
